13609e24fdc048ef6143c7e673423c177eb24e1d
Marina Mel <melm2004@mail.ru>
2023-11-29 16:24:19 +0300

    done Issue

---
+++ WebLibrary/Consumers/Issue/CreateIssueConsumer.cs
@@ -0,0 +1,22 @@
+﻿using MassTransit;
+using ServiceModels.Requests.Issue;
+using ServiceModels.Responses.Issue;
+
+namespace WebLibrary.Consumers.Issue;
+
+public class CreateIssueConsumer : IConsumer<CreateIssueRequest>
+{
+    private readonly ICreaterIssue _command;
+
+    public CreateIssueConsumer(ICreaterIssue command)
+    {
+        _command = command;
+    }
+
+    public async Task Consume(ConsumeContext<CreateIssueRequest> context)
+    {
+        CreateIssueResponse actionResult = await _command.CreateAsync(context.Message);
+
+        await context.RespondAsync(actionResult);
+    }
+}
