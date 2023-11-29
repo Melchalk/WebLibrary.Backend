94b285fb62ad4e0df6429b0812fed75d0e8eee36
Marina Mel <melm2004@mail.ru>
2023-11-29 17:50:38 +0300

    fix book

--- WebLibrary/Validators/Book/CreateBookRequestValidator.cs
+++ WebLibrary/Validators/Book/CreateBookRequestValidator.cs
@@ -1,12 +1,20 @@
 ﻿using FluentValidation;
-using ServiceModels.Requests.Issue;
+using ServiceModels.Requests.Book;
 
-namespace WebLibrary.Validators.Issue;
-
-public class CreateIssueRequestValidator : AbstractValidator<CreateIssueRequest>, ICreateIssueRequestValidator
+namespace WebLibrary.Validators.Book;
+public class CreateBookRequestValidator : AbstractValidator<CreateBookRequest>, ICreateBookRequestValidator
 {
-    public CreateIssueRequestValidator()
+    public CreateBookRequestValidator()
     {
+        RuleFor(request => request.Title)
+          .NotEmpty();
 
+        RuleFor(request => request.NumberPages)
+          .Must(a => a > 0)
+          .WithMessage("Number of pages must be positive");
+
+        RuleFor(request => request.YearPublishing)
+          .Must(a => a > 0)
+          .WithMessage("Year of publishing must be positive");
     }
 }
--- WebLibrary/Validators/Issue/CreateIssueRequestValidator.cs
+++ WebLibrary/Validators/Issue/CreateIssueRequestValidator.cs
@@ -7,15 +7,6 @@ public class CreateIssueRequestValidator : AbstractValidator<CreateBookRequest>,
 {
     public CreateIssueRequestValidator()
     {
-        RuleFor(request => request.Title)
-          .NotEmpty();
 
-        RuleFor(request => request.NumberPages)
-          .Must(a => a > 0)
-          .WithMessage("Number of pages must be positive");
-
-        RuleFor(request => request.YearPublishing)
-          .Must(a => a > 0)
-          .WithMessage("Year of publishing must be positive");
     }
 }
