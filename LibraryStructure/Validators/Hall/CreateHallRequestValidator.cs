dce947610089f98e2a858ca59ad3b89e5221d86e
Marina Mel <melm2004@mail.ru>
2023-12-02 01:54:05 +0300

    add validators for hall and issue

--- LibraryStructure/Validators/Hall/CreateHallRequestValidator.cs
+++ LibraryStructure/Validators/Hall/CreateHallRequestValidator.cs
@@ -6,6 +6,8 @@ public class CreateHallRequestValidator : AbstractValidator<CreateHallRequest>, 
 {
     public CreateHallRequestValidator()
     {
-
+        RuleFor(request => request.No)
+            .Must(a => a > 0)
+            .WithMessage("Number of hall must be positive");
     }
 }
